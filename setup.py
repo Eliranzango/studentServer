a42b73236609e4bace393c73330b92cbeb5f588e
Eliranzango <eliranzangom@gmail.com>
2020-09-26 20:48:47 +0300

    fixed filters

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='flask-sqlite-master',
+    version='1.0',
+    packages=['app'],
+    url='',
+    license='',
+    author='elira',
+    author_email='',
+    description=''
+)
